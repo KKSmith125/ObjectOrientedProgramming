3fcbbffdb599164d8873a519012e06695ad7cb88
Kaylee <ks0754@mcla.edu>
2025-09-24 10:31:10 -0400

    finished draft

---
+++ labs/command_pattern/Light/LightOnCommand.java
@@ -0,0 +1,19 @@
+package labs.command_pattern.Light;
+
+import labs.command_pattern.Command;
+
+public class LightOnCommand implements Command {
+    Light light;
+
+    public LightOnCommand(Light light) {
+        this.light = light;
+    }
+
+    public void execute() {
+        light.on();
+    }
+
+    public void undo() {
+        light.off();
+    }
+}
