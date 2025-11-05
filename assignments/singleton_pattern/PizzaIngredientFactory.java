4b7e87bf93867bd8d0553b034f4a7deacfc9818b
Kaylee <ks0754@mcla.edu>
2025-11-04 20:54:00 -0500

    base code to merge

---
+++ assignments/singleton_pattern/PizzaIngredientFactory.java
@@ -0,0 +1,16 @@
+package assignments.singleton_pattern;
+
+import assignments.singleton_pattern.ingredients.*;
+
+public interface PizzaIngredientFactory {
+
+    public Dough createDough();
+
+    public Sauce createSauce();
+
+    public Cheese createCheese();
+
+    public Pepperoni createPepperoni();
+
+    public Parmesan createParmesan();
+}
