90cb1423d2e9f40a2411e0b2048ae26f1dd754f7
Kaylee <ks0754@mcla.edu>
2025-11-09 18:17:16 -0500

    logger fix

--- assignments/singleton_pattern/SingletonLogger.java
+++ assignments/singleton_pattern/SingletonLogger.java
@@ -5,7 +5,7 @@ import java.time.format.DateTimeFormatter;
 
 public class SingletonLogger {
     private static SingletonLogger singletonLoggerInstance;
-    String level;
+    String level = "INFO";
     String statement;
 
     private SingletonLogger() {
@@ -15,11 +15,28 @@ public class SingletonLogger {
         this.level = level;
     }
 
-    public void log(String level, String statement) {
-        LocalDateTime now = LocalDateTime.now();
-        String timestamp = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    private int getLevelNumber(String level) {
+        if (level.equals("INFO")) {
+            return 1;
+        } else if (level.equals("WARN")) {
+            return 2;
+        } else if (level.equals("CRITICAL")) {
+            return 3;
+        } else {
+            return 0;
+        }
+    }
 
-        System.out.println("[" + timestamp + "] [" + level + "]" + statement);
+    public void log(String level, String statement) {
+        int currentLevel = getLevelNumber(this.level);
+        int newLevel = getLevelNumber(level);
+
+        if (newLevel >= currentLevel) {
+            LocalDateTime now = LocalDateTime.now();
+            String timestamp = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
+            System.out.println("[" + timestamp + "] [" + level + "]" + statement);
+        }
     }
 
     public static SingletonLogger getInstance() {
